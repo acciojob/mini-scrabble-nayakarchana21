df25fe353a76d7ff4fe4d02b97b086bf99c379f9
Archana Nayak <archananayak212002@gmail.com>
2025-06-17 13:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your code here
+document.getElementById('evaluatedText').addEventListener('input', (e) => {
+  const text = e.target.value.trim();
+  document.getElementById('letterCount').innerHTML = text.length;
+});
